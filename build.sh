3900409974d8a0a813a9839af54015c4df1cadb1
Stephen Meriwether <smeriwether@hey.com>
2021-01-08 23:17:07 -0500

    Add a production deploy script

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+rm -rf ./site
 
 npx postcss ./styles.css -o ./site/styles.css
 
 cp index.html ./site/index.html
 cp thanks.html ./site/thanks.html
 cp 404.html ./site/404.html
+
+if [ $NODE_ENV == "production" ]; then
+  for file in ./site/*.html; do
+    mv -- "$file" "${file%%.html}"
+  done
+fi
 
 cp -R ./public ./site
