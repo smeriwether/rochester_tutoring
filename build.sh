c5a210ec13d6b41653e1ad52b634619e63ba021b
Stephen Meriwether <smeriwether@hey.com>
2021-05-05 09:22:18 -0400

    Fix list item dot on mobile

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cp index.html ./site/index.html
 cp thanks.html ./site/thanks.html
 cp 404.html ./site/404.html
 
-if [ $NODE_ENV == "production" ]; then
+if [[ $NODE_ENV == "production" ]]; then
   for file in ./site/*.html; do
     mv -- "$file" "${file%%.html}"
   done
