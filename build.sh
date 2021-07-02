c4f954f3fb00f76b24d92ad0e8931859cd0aeefd
Stephen Meriwether <smeriwether@hey.com>
2021-07-02 09:33:33 -0400

    Move to Render

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./site
 
-npx postcss ./styles.css -o ./site/styles.css
+npx postcss --config=./postcss.config.js ./styles.css -o ./site/styles.css
 
 cp index.html ./site/index.html
 cp thanks.html ./site/thanks.html
